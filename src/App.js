02dd438050ff94a08cc7ffdd857c430a7dbf4266
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-04-03 14:44:54 -0400

    Component - App - show city weather fetched data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
+import { useState, useEffect } from 'react'
+import { fetchWeather } from './api/fetchWeather'
+
 const App = () => {
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({})
+  const [isPending, setIsPending] = useState(true)
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const data = await fetchWeather('Oslo')
+      setWeather(data)
+      setIsPending(false)
+    }, 1000)
+  }, [])
+
+  const search = async (e) =>{
+    if (city.trim() === '') return
+    if (e.key === 'Enter') {
+      try {
+        const data = await fetchWeather(city)
+        console.log(data)
+        setWeather(data)
+        setCity('')
+      } catch (e) { console.log(e) }
+    }
+  }
+
   return (
-    <>
-      <h1>Hello from React</h1>
-    </>
+    <div className="main">
+      <input
+        className="main__element main__search-box"
+        name="weather"
+        placeholder="Buscar...."
+        value={city}
+        onChange={e => setCity(e.target.value)}
+        onKeyPress={search}
+        autoComplete="off"
+      />
+      <div className="main__element main__content">
+        { isPending ? (
+          <div className="loader">
+            <div className="loader__center"></div>
+          </div>
+        ) : '' }
+        { weather.main ? (
+          <>
+            <div className="content__city">
+              <p className="city-name">{ weather.name }</p>
+              <span className="city-label">{ weather.sys.country }</span>
+            </div>
+            <h3 className="city-temp">{ parseInt(weather.main.temp, 10) }</h3>
+            <p className="city-weather-description">{ weather.weather[0].description }</p>
+          </>
+        ) : '' }
+      </div>
+    </div>
   )
 }
 
