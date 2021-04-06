d8c18b6a421ea53e2e665700a85936633474e21f
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-04-06 16:47:31 -0400

    Project - api - setting fetchWeather to return a promise

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,15 +1,13 @@
 import axios from 'axios'
+const appid = 'f33a484cf794d08d0148764789aaba32'
 
 export const fetchWeather = async (cityName) => {
-  const { data } = await axios.get(
-    'https://api.openweathermap.org/data/2.5/weather',
-    {
-      params: {
-        q: cityName,
-        units: 'metric',
-        appid: 'f33a484cf794d08d0148764789aaba32'
-      }
-    }
-  )
-  return data
+  return new Promise(async (resolve, reject) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appid}&units=metric`
+      const response = await axios.get(url)
+      const { data } = response
+      resolve(data)
+    } catch (e) { reject({}) }
+  }) // PROMISE END
 }
